35a4f40e2c14df15f29681d53e8c73e5769e36be
Abhijeet Dhumal <84722973+abhijeet-dhumal@users.noreply.github.com>
2022-02-17 16:42:03 +0530

    Update models.py

--- form/models.py
+++ form/models.py
@@ -80,8 +80,8 @@ class Appointment(models.Model):
     doctor = models.ForeignKey(Doctor, null=True, on_delete= models.CASCADE)
     patient = models.ForeignKey(Patient, null=True, on_delete= models.CASCADE)
     speciality=models.CharField("Required speciality ", max_length=50, blank=True)
-    # date_of_appointment=models.DateField("Date of appointment ", max_length=50, blank=True)
-    Starttime_of_appointment=models.DateTimeField("Start-time of appointment ", max_length=50, blank=True)
+    date_of_appointment=models.DateField("Date of appointment ", max_length=50, blank=True)
+    Starttime_of_appointment=models.TimeField("Start-time of appointment ", max_length=50, blank=True)
 
     def __str__(self):
         if self.speciality==None:
